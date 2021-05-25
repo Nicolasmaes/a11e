89408bd287718e9f887bca5ccbbf61374858dc81
luc <l.leneuf@codeur.online>
2021-05-25 16:58:29 +0200

    start

---
+++ pages/landing_page.php
@@ -0,0 +1,47 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="../styles/styles.css">
+</head>
+
+<body>
+
+    <div class="margin">
+
+        <header>
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </header>
+
+        <main>
+            <p>
+            Découvrons ensemble une communauté qui n'est pas assez entendue
+            </p>
+            
+            <div class="video"></div>
+
+            <div class="miniature">
+                <p class="miniature__text">
+                    <span>Interview</span> <br>
+                    <span>A l'écoute des sourds</span>
+                </p>
+
+                <img src="../dossier-conception/futures-assets/logo-landing-page-mic.png" alt="illustration de micro">
+            </div>
+
+            <div class="miniature">
+                <p class="miniature__text">
+                </p>
+            </div>
+        </main>
+
+        <footer>
+        </footer>
+    
+    </div>
+    
+</body>
+</html>
