defca4449c0ca34753834b7c8b5eea004adbbac4
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-05-27 14:49:50 +0200

    update stat.php and stat.scc

---
+++ stat.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="statistique obtenu ">
+        <title>statistique</title>
+        <link rel="stylesheet" href="styles/stat.css">
+    </head>
+    <body>
+        <div class="container">
+            <span class="title">Une France plus sourde qu’on ne le croit !</span>
+
+            <div class="stat-p">
+                <p>
+                    <span class="subtitle">Des chiffres globaux</span><br>
+
+                    Si 182 000 personnes se définissent comme ayant une surdité complète (0.3 % de la population totale), plus de 7 millions de personnes reconnaissent avoir au moins une déficience auditive soit 11.2 % des français.<br>  
+
+                    Autour de 11.5 % des personnes reconnaissent avoir des difficultés à entendre une conversation avec plusieurs personnes.<br>
+
+                    En prenant l’angle des limitations fonctionnelles auditives, l’enquête permet d’établir une estimation de 10 millions de personnes concernées.  
+
+                    <ul> 
+                        <li>
+                            - 10 millions de personnes ont des problèmes d’audition soit 16 % de la population française.
+                        </li>
+                        <li>
+                            - Pour 5,4 millions de ces personnes, il y a des répercussions sur la vie quotidienne soit 8,6 % de la population française.
+                        </li>
+                        <li>
+                            - Après 50 ans, une personne sur trois ont des difficultés auditives, et plus d’une sur deux après 80 ans.
+                        </li>
+                        <li>
+                            - Un enfant sur 1000 naît avec une déficience auditive ou est dépisté avant deux ans soit 700 enfants par an.
+                        </li>
+                        <li>
+                            - 6,6 % de la population française (4,09 millions de personnes) souffrent d’un déficit auditif, dont 88 % sont devenus sourds au cours de leur vie.
+                        </li>
+                        <li>
+                            - 483 000 personnes avec une déficience auditive profonde ou sévère.
+                        </li>
+                        <li>
+                            - 600 000 malentendants portent un appareil auditif.
+                        </li>
+                        <li>
+                            - 80 000 utilisent la LSF.
+                        </li>
+                    </ul>
+                </p>
+            </div>
+
+            <div class="stat-p">
+                <p>
+                    <span class="subtitle">L’appareillage</span><br>
+                    20 % des personnes ayant des limitations fonctionnelles auditives moyennes à totales sont équipées d’un appareil auditif.<br><br>
+
+                    Plus de 2 millions de personnes ne portent pas d’appareils mais en auraient besoin.<br><br>
+
+                    L’enquête note une forte augmentation du taux d’équipement entre 1998 et 2008. Les personnes de moins de 20 ans sont plus souvent équipées, à limitation fonctionnelle égale.
+                </p>
+            </div>
+
+            <div class="stat-p">
+                <p>
+                    <span class="subtitle">Vie sociale</span><br>
+                    L’enquête ne note pas de différences significatives sur la vie affective en cas de troubles auditifs, ni de fortes entraves sur les relations familiales ou amicales.<br><br> 
+
+                    Les pratiques de loisirs sont semblables entre personnes ayant des déficiences auditives et personnes n’ayant aucune difficulté. Seules les personnes ayant des limitations fonctionnelles très graves à totales semblent avoir une activité de loisirs plus réduite. Cette donnée est à corréler avec l’âge et le niveau de diplômes des personnes interrogées.<br><br>
+
+                    L’enquête relève toutefois une forte proportion de jeunes sourds de naissance ne pratiquant pas de sport, ce qui pourrait indiquer des difficultés d’insertion sociale.
+                </p>
+            </div>
+
+            <div class="stat-p">
+                <p>
+                    <span class="subtitle">Le cinéma</span><br>
+                    Vive le cinéma étranger ! Cela peut paraître un paradoxe, mais le cinéma français est quasiment inaccessible aux sourds.<br><br>
+
+                    Les seuls films disponibles avec des sous-titrages sont les films en langues étrangères diffusés en version originale. Par définition, en France on ne sous-titre pas les films en langue française au dépends des sourds. <br><br>
+
+                    En outre, à ma connaissance, mis à part à Paris, il existe très peu de salles diffusant des films en version originale. Et par conséquent, ce n’est pas un hasard que les sourds désertent les salles obscures en France.  
+                </p>
+            </div>
+
+            <div class="stat-p">
+                <p>
+                    <span class="subtitle">Situation administrative</span><br>
+
+                    Parmi les personnes ayant des limitations auditives moyennes à totales, 16 % déclarent avoir une reconnaissance administrative de leur handicap (taux d’incapacité reconnu par la MDPH, ou taux d’invalidité octroyé par la Sécurité Sociale par exemple).<br><br>  
+
+                    Les personnes de moins de 60 ans et ayant une limitation fonctionnelle auditive ont plus souvent une reconnaissance administrative du handicap. A difficultés égales, les femmes ont une plus faible reconnaissance de leur handicap.<br><br>
+
+                    4 % des personnes ayant des limitations fonctionnelles moyennes à totales touchent une pension d’invalidité. 5 % des personnes entre 20 et 59 ans et ayant des limitations fonctionnelles moyennes à totales déclarent recevoir l’Allocation Adulte Handicapé (AAH). <br><br>
+
+                    Liens utilisé : <br><br>
+                    <a href="https://www.surdi.info/bibliographie/des-chiffres-autour-de-la-surdite/#:~:text=Des%20chiffres%20globaux,auditive%20soit%2011.2%20%25%20des%20fran%C3%A7ais.">Des chiffres autour de la surdité</a>
+                </p>
+            </div>
+
+        </div>
+        
+    </body>
+</html>
