2b91b8f49c49659be0086eb682a533e21357d373
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-05-27 16:17:12 +0200

    utpade mention legale

---
+++ m-leg.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="La page pour mention légale">
+        <title>Document</title>
+        <link rel="stylesheet" href="styles/m-leg.css">
+    </head>
+    <body>
+        <div class="container">
+            <p class="general">
+                <span class="leg-title"> GNU GENERAL PUBLIC LICENSE</span><br>
+
+                <span class="version"> Version 3, 29 June 2007</span><br><br>
+
+                Copyright (C) 2007 Free Software Foundation, Inc. <a href=https://fsf.org></a>
+                Everyone is permitted to copy and distribute verbatim copies
+                of this license document, but changing it is not allowed.
+            </p>
+
+            <p class="preamble">
+                <span class="sbt-preamble">Preamble</span> <br><br>
+
+                The GNU General Public License is a free, copyleft license for software and other kinds of works. <br>
+                The licenses for most software and other practical works are designed to take away your freedom to share and change the works.  By contrast,
+                the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free
+                software for all its users.  We, the Free Software Foundation, use the GNU General Public License for most of our software; it applies also to
+                any other work released this way by its authors.  You can apply it to your programs, too. <br><br>
+
+                When we speak of free software, we are referring to freedom, not price.  Our General Public Licenses are designed to make sure that you have the freedom to distribute copies of free software (and charge for them if you wish), that you receive source code or can get it if you want it, that you can change the software or use pieces of it in new free programs, and that you know you can do these things. <br><br>
+
+                To protect your rights, we need to prevent others from denying you these rights or asking you to surrender the rights.  Therefore, you have certain responsibilities if you distribute copies of the software, or if you modify it: responsibilities to respect the freedom of others. <br><br>
+
+                For example, if you distribute copies of such a program, whether gratis or for a fee, you must pass on to the recipients the same freedoms that you received.  You must make sure that they, too, receive or can get the source code.  And you must show them these terms so they know their rights. <br><br>
+
+                Developers that use the GNU GPL protect your rights with two steps: (1) assert copyright on the software, and (2) offer you this License giving you legal permission to copy, distribute and/or modify it. <br><br>
+
+                For the developers' and authors' protection, the GPL clearly explains that there is no warranty for this free software.  For both users' and authors' sake, the GPL requires that modified versions be marked as changed, so that their problems will not be attributed erroneously to authors of previous versions. <br><br>
+
+                Some devices are designed to deny users access to install or run modified versions of the software inside them, although the manufacturer can do so.  This is fundamentally incompatible with the aim of protecting users' freedom to change the software.  The systematic pattern of such abuse occurs in the area of products for individuals to use, which is precisely where it is most unacceptable.  Therefore, we have designed this version of the GPL to prohibit the practice for those products.  If such problems arise substantially in other domains, we stand ready to extend this provision to those domains in future versions of the GPL, as needed to protect the freedom of users. <br><br>
+
+                Finally, every program is threatened constantly by software patents. States should not allow patents to restrict development and use of software on general-purpose computers, but in those that do, we wish to avoid the special danger that patents applied to a free program could make it effectively proprietary.  To prevent this, the GPL assures that patents cannot be used to render the program non-free. <br><br>
+
+                The precise terms and conditions for copying, distribution and modification follow. <br><br>
+
+            </p>
+
+            <p class="terms">
+                <span>TERMS AND CONDITIONS</span>
+
+                <ol>
+                    <li>Definitions.
+                        <p>
+                            "This License" refers to version 3 of the GNU General Public License. <br>
+                            "Copyright" also means copyright-like laws that apply to other kinds of works, such as semiconductor masks. <br>
+                            "The Program" refers to any copyrightable work licensed under this License.  Each licensee is addressed as "you".  "Licensees" and "recipients" may be individuals or organizations. <br>
+                            To "modify" a work means to copy from or adapt all or part of the work in a fashion requiring copyright permission, other than the making of an exact copy.  The resulting work is called a "modified version" of the earlier work or a work "based on" the earlier work. <br>
+                            A "covered work" means either the unmodified Program or a work based on the Program. <br>
+                            To "propagate" a work means to do anything with it that, without permission, would make you directly or secondarily liable for infringement under applicable copyright law, except executing it on a computer or modifying a private copy.  Propagation includes copying, distribution (with or without modification), making available to the public, and in some countries other activities as well. <br>
+                            To "convey" a work means any kind of propagation that enables other parties to make or receive copies.  Mere interaction with a user through a computer network, with no transfer of a copy, is not conveying. <br><br>
+                            An interactive user interface displays "Appropriate Legal Notices" to the extent that it includes a convenient and prominently visible feature that (1) displays an appropriate copyright notice, and (2) tells the user that there is no warranty for the work (except to the extent that warranties are provided), that licensees may convey the work under this License, and how to view a copy of this License.  If the interface presents a list of user commands or options, such as a menu, a prominent item in the list meets this criterion. <br>
+                        </p>
+                    </li>
+                    <li>Source Code.
+                        <p>
+                            The "source code" for a work means the preferred form of the work for making modifications to it.  "Object code" means any non-source form of a work. <br>
+                            A "Standard Interface" means an interface that either is an official standard defined by a recognized standards body, or, in the case of interfaces specified for a particular programming language, one that is widely used among developers working in that language. <br>
+                            The "System Libraries" of an executable work include anything, other than the work as a whole, that (a) is included in the normal form of packaging a Major Component, but which is not part of that Major Component, and (b) serves only to enable use of the work with that Major Component, or to implement a Standard Interface for which an implementation is available to the public in source code form.  A "Major Component", in this context, means a major essential component (kernel, window system, and so on) of the specific operating system (if any) on which the executable work runs, or a compiler used to produce the work, or an object code interpreter used to run it. <br>
+                            The "Corresponding Source" for a work in object code form means all the source code needed to generate, install, and (for an executable work) run the object code and to modify the work, including scripts to control those activities.  However, it does not include the work's System Libraries, or general-purpose tools or generally available free programs which are used unmodified in performing those activities but which are not part of the work.  For example, Corresponding Source includes interface definition files associated with source files for the work, and the source code for shared libraries and dynamically linked subprograms that the work is specifically designed to require, such as by intimate data communication or control flow between those subprograms and other parts of the work. <br>
+                            The Corresponding Source need not include anything that users can regenerate automatically from other parts of the Corresponding Source. <br>
+                            The Corresponding Source for a work in source code form is that same work. <br>
+                        </p>
+                    </li>
+                    <li>Basic Permissions.
+                        <p>
+                            All rights granted under this License are granted for the term of copyright on the Program, and are irrevocable provided the stated conditions are met.  This License explicitly affirms your unlimited permission to run the unmodified Program.  The output from running a covered work is covered by this License only if the output, given its content, constitutes a covered work.  This License acknowledges your rights of fair use or other equivalent, as provided by copyright law. <br>
+                            You may make, run and propagate covered works that you do not convey, without conditions so long as your license otherwise remains in force.  You may convey covered works to others for the sole purpose of having them make modifications exclusively for you, or provide you with facilities for running those works, provided that you comply with the terms of this License in conveying all material for which you do not control copyright.  Those thus making or running the covered worksfor you must do so exclusively on your behalf, under your direction and control, on terms that prohibit them from making any copies of your copyrighted material outside their relationship with you. <br>
+                            Conveying under any other circumstances is permitted solely under the conditions stated below.  Sublicensing is not allowed; section 10 makes it unnecessary. <br>
+                        </p>
+                    </li>
+                    <li>Protecting Users' Legal Rights From Anti-Circumvention Law.
+                        <p>
+                        No covered work shall be deemed part of an effective technological measure under any applicable law fulfilling obligations under article 11 of the WIPO copyright treaty adopted on 20 December 1996, or similar laws prohibiting or restricting circumvention of such measures. <br>
+                        When you convey a covered work, you waive any legal power to forbid circumvention of technological measures to the extent such circumvention is effected by exercising rights under this License with respect to the covered work, and you disclaim any intention to limit operation or modification of the work as a means of enforcing, against the work's users, your or third parties' legal rights to forbid circumvention of technological measures. <br>
+                        </p>
+                    </li>
+                    <li>Conveying Verbatim Copies.
+                        <p>
+                            You may convey verbatim copies of the Program's source code as you receive it, in any medium, provided that you conspicuously and appropriately publish on each copy an appropriate copyright notice; keep intact all notices stating that this License and any non-permissive terms added in accord with section 7 apply to the code; keep intact all notices of the absence of any warranty; and give all recipients a copy of this License along with the Program.
+                        </p>
+                    </li>
+                    <li>Conveying Modified Source Versions.</li>
+                    <li>
+                        Conveying Non-Source Forms.
+                        <ul>
+                            <li>
+                                a) Convey the object code in, or embodied in, a physical product (including a physical distribution medium , accompanied by the Corresponding Source fixed on a durable physical medium customarily used for software interchange.
+                            </li>
+                            <li>
+                                b) Convey the object code in, or embodied in, a physical product (including a physical distribution medium), accompanied by a written offer, valid for at least three years and valid for as long as you offer spare parts or customer support for that product model, to give anyone who possesses the object code either (1) a copy of the Corresponding Source for all the software in the product that is covered by this License, on a durable physical medium customarily used for software interchange, for a price no more than your reasonable cost of physically performing this conveying of source, or (2) access to copy the Corresponding Source from a network server at no charge.
+                            </li>
+                            <li>
+                                c) Convey individual copies of the object code with a copy of the written offer to provide the Corresponding Source.  This alternative is allowed only occasionally and noncommercially, and only if you received the object code with such an offer, in accord with subsection 6b.</li>
+                            <li>
+                                d) Convey the object code by offering access from a designated place (gratis or for a charge), and offer equivalent access to the Corresponding Source in the same way through the same place at no further charge.  You need not require recipients to copy the Corresponding Source along with the object code.  If the place to copy the object code is a network server, the Corresponding Source may be on a different server (operated by you or a third party) that supports equivalent copying facilities, provided you maintain clear directions next to the object code saying where to find the Corresponding Source.  Regardless of what server hosts the Corresponding Source, you remain obligated to ensure that it is available for as long as needed to satisfy these requirements.
+                            </li>
+                            <li>
+                                e) Convey the object code using peer-to-peer transmission, provided you inform other peers where the object code and Corresponding Source of the work are being offered to the general public at no charge under subsection 6d.</li>
+                            <li></li>
+                        </ul>
+                    </li>
+                    <li>
+                        A separable portion of the object code, whose source code is excluded from the Corresponding Source as a System Library, need not be included in conveying the object code work.</li>
+                    <li>
+                        A "User Product" is either (1) a "consumer product", which means any tangible personal property which is normally used for personal, family, or household purposes, or (2) anything designed or sold for incorporation into a dwelling.  In determining whether a product is a consumer product, doubtful cases shall be resolved in favor of coverage.  For a particular product received by a particular user, "normally used" refers to a typical or common use of that class of product, regardless of the status of the particular user or of the way in which the particular user actually uses, or expects or is expected to use, the product.  A product is a consumer product regardless of whether the product has substantial commercial, industrial or non-consumer uses, unless such uses represent the only significant mode of use of the product.
+                    </li>
+                    <li>
+                        "Installation Information" for a User Product means any methods, procedures, authorization keys, or other information required to install and execute modified versions of a covered work in that User Product from a modified version of its Corresponding Source.  The information must suffice to ensure that the continued functioning of the modified object code is in no case prevented or interfered with solely because modification has been made.
+                    </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </p>
+
+
+
+            
+
+            
+
+
+        </div>
+    </body>
+</html>
