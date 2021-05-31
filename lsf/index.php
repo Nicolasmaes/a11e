4031d8781022943622e4e37034873f4f4a33fe59
sonia <s.rolland@codeur.online>
2021-05-31 16:44:53 +0200

    text-correction

--- lsf/index.php
+++ lsf/index.php
@@ -5,90 +5,104 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Language des signes</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" media="screen" href="style.css">
 </head>
 <body>
-     <div class="main_lsf">
-
-         <h1>accessibilité numérique et langue des signes</h1>
-   
-    <p>Lors de l'élaboration d'un projet sur l'accessibilité numérique orientée
-     sur la surdité,<br> la question s'est posé de savoir quels était les meilleurs façons
-     de retranscrire<br> sons et paroles dans des contenus audio et vidéos.
-    <br><br> Les sous-titres et les closed captions sont traités dans un autre article.<br> <a href="https://promo-68.codeur.online/blog/index.php/author/nicolasm/">Ecris par NICOLASM</a>
-
-    </p>
-
-       <h2>historique de la LSF</h2>
-    <p>
-    La LSF (langue des signes française) a longtemps été combattu et même explicitement <br>interdite, 
-    notamment car perçu par les autorités comme un "patois gestuel" (voir loi <br>du congrès de  Milan de 1880).
-    Ce n'est qu'en 1991 que l'interdiction de la LSF est<br> levé dans l'éducation, 
-    et celle-ci sera reconnut officiellement comme  une langue<br> à part entière en 2005.
-    </p>
-
-     <h3>spécificités de la LSF</h3>
-   
-    <h4>Alphabet dactylologique</h4>
-    <p>L’alphabet dactylologique est la façon de signer l’alphabet latin. Il est 
-     utilisé pour épeler<br> les noms propres ou les mots n’existant pas encore en LSF.
-      La dactylologie de la<br> LSF se fait d’une seule main.</p>
-     <img src="img/langue-des-signes.jpg" class="image">
-    
-         <h5>grammaire de la LSF</h5>
-     <p>
-     La langue des signes française permet d'exprimer plusieurs idées simultanément,<br>
-      ce qui la différencie de la grammaire française classique.
-      <li>L'expressions du visage peut indiquer le sens de la phrase (par exemple pour <br>dire qu'il s'agit d'une question).</li>
-      <li>Il n’existe pas de conjugaison en LSF, il suffit au signeur de situer l’action sur<br> la ligne du temps 
-      (perpendiculaire à lui:  derrière son épaule le passé,<br> au niveau de son corps le présent et devant lui le futur).</li>
-      <li>Des gestes montres le rôle ou la forme du référent: Pour faire  référence à un <br>homme, le locuteur utilisera son index vers le  haut,mais pour parler d’une <br>voiture, il utilisera la main à plat. Les verbes de mouvement peuvent <br>également être  signés pour indiquer  la direction :
-         « La voiture tourne à <br>gauche »  sera le classificateur de voiture tournant à gauche.<br> Autre exemple,« Un homme monte l’escalier » se fera par deux doigts
-           imitant<br> les jambes montant un escalier, dans un mouvement pouvant être différent<br> selon qu’il s’agit d’un escalier classique ou en colimaçon.</li>
-      <li>Pour parler de personnes absentes le signeur définit sa place et celle des autres <br>dans l’espace, à la  manière d’une mise en scène théâtrale, il est ainsi plus facile<br> de  comprendre et de suivre visuellement de qui il s’agit et quels sont les <br>rapports entre les personnages.</li>
-          En bref, la LSF est très riche et permet une communication très complète,<br> qui viens en quelque sorte combler par le visuel le manque à gagner auditif.
-        </p>
-
-         <h6>incrustation vidéo</h6>
-         <p>
-         Même si on est très loin d'une pratique systématique, on a tous déjà vu des vidéos,<br> notamment de discours officiels,
-          avec incrustation dans un coin de l'écran d'une<br> personne traduisant les dialogues en temps réel. 
-         <br><br>Comme vu précédemment, cela permet a priori de retranscrire plus fidèlement le <br>discours que par les sous-titres.
-         En effet, et notamment dans le cadre d'une<br> traduction instantanée, dure d'imaginer retranscrire par des  sous-titres,<br>
-         les subtilités de la langue parlée.
-         Tout simplement car l'écrit n'est pas fait<br> pour cela : le descriptif complet des subtilités du language oral, incluant<br> les intonations par exemple, serait trop long pour du temps réel.
-         </p>
-
-          <h7>applications utilisant la langue des signes</h7>
-         <p>
-         Pour l'instant, il n'existe pas ou peut d'application pour sourds utilisant la langue<br> des signes.
-         Il existe en revanche des  applications de sous-titrage (comme<br> rogervoice ou ava)
-          ou des applications de traîtement de son (a destination <br>des mal-entendants).
-         <br><br>Cela est sans doute dû au fait que retranscrire une conversationen LSF via un  <br>algorithme serait
-         techniquement plus compliquer que  généré des sous-titres :<br>  en effet, il faudrait pour cela analyser
-         des information autre que les seuls mots,<br>  et la traduction passerait non-pas par des caractères mais
-          par exemple par une<br> animation complexe, impliquant des mouvement de main mais aussi de tout 
-         <br>le corps, incluant des expressions faciales.
-        </p> 
-
-         <h8>idées pour le projet << accessibilité numérique >></h8>
+        <div class="main_lsf">
+        <div class="test">
+            <h2> accessibilité numérique et langue des signes </h2>
         
-        <p> Une idée serait de créer une interface semblable à celle d'un traducteur
-         comme <br>celui de google, qui traduirait en alphabet dactylologique.
-        <br><br>Celui-ci serait principalement à destination des personnes entendantes ayant <br>
-        besoin d'un coup de pouce pour communiquer avec une personne sourde.
-        <br><br>Un tel traducteur permettrait de constituer un mot bien plus rapidement qu'en 
-        <br>ayant sous les yeux l'habituel fiche alphabétique.
-        </p>
+            <p class="test_para">
+                Lors de l'élaboration d'un projet sur l'accessibilité numérique orientée sur la surdité,<br> la question s'est posé de savoir quels était les meilleurs façons de retranscrire sons<br> et paroles dans des contenus audio et vidéos.<br><br> 
+                Les sous-titres et les closed captions sont traités dans un autre article.<br>
+                <a href="https://promo-68.codeur.online/blog/index.php/author/nicolasm/">Ecris par NICOLASM</a>
 
-        <h9>Source</h9><br>
-       <a href="https://lejournal.cnrs.fr/articles/faciliter-la-traduction-en-langue-des-signes">facilité de la traducion en langue des signes</a><br>
-       <a href="http://lsq.uqam.ca/sites/default/files/Banff2003.pdf">logiciel d’apprentissage s’adressant à des personnes sourdes et pour la création de ce logiciel</a><br>
-        <a href="https://fr.wikipedia.org/wiki/Langue_des_signes_française">langue des signes française</a><br>
-        <a href="https://webzine.okeenea.com/6-applications-sourds-malentendants-2020/">6 applications pour les personnes sourdes et malentedantes en 2020</a>
-      
+            </p>
+          </div>
+
+            <div class="test">
+        
+                <h2>historique de la LSF</h2>
+                <p class="test_para">
+                    La LSF (langue des signes française) a longtemps été combattu et même explicitement <br>interdite, 
+                    notamment car perçu par les autorités comme un "patois gestuel" (voir loi <br>du congrès de  Milan de 1880).
+                    Ce n'est qu'en 1991 que l'interdiction de la LSF est<br> levé dans l'éducation, 
+                    et celle-ci sera reconnut officiellement comme  une langue<br> à part entière en 2005.
+                </p>
+            </div>
+
+            <div class="test">
+            <h2>spécificités de la LSF</h2>
+        
+            <h4>Alphabet dactylologique</h4>
+            <p class="test_para">
+                L’alphabet dactylologique est la façon de signer l’alphabet latin. Il est utilisé pour épeler<br> les noms propres ou les mots n’existant pas encore en LSF.La dactylologie de la<br> LSF se fait d’une seule main.
+            </p>
+            </div>
+
+                <img src="img/langue-des-signes.jpg" class="image">
+                
+                <div class="test">
+            <h2>grammaire de la LSF</h2>
+            <p class="test_para">
+                La langue des signes française permet d'exprimer plusieurs idées simultanément,ce qui<br> la différencie de la grammaire française classique.
+            <li>-L'expressions du visage peut indiquer le sens de la phrase (par exemple pour <br>dire qu'il s'agit d'une question).</li>
+            <li>-Il n’existe pas de conjugaison en LSF, il suffit au signeur de situer l’action sur<br> la ligne du temps 
+            (perpendiculaire à lui:  derrière son épaule le passé,<br> au niveau de son corps le présent et devant lui le futur).</li>
+            <li>-Des gestes montres le rôle ou la forme du référent: Pour faire  référence à un <br>homme, le locuteur utilisera son index vers le  haut,mais pour parler d’une <br>voiture, il utilisera la main à plat. Les verbes de mouvement peuvent <br>également être  signés pour indiquer  la direction :
+                « La voiture tourne à <br>gauche »  sera le classificateur de voiture tournant à gauche.<br> -Autre exemple,« Un homme monte l’escalier » se fera par deux doigts
+                imitant<br> les jambes montant un escalier, dans un mouvement pouvant être différent<br> selon qu’il s’agit d’un escalier classique ou en colimaçon.</li>
+            <li>-Pour parler de personnes absentes le signeur définit sa place et celle des autres <br>dans l’espace, à la  manière d’une mise en scène théâtrale, il est ainsi plus facile<br> de  comprendre et de suivre visuellement de qui il s’agit et quels sont les <br>rapports entre les personnages.</li><br>
+           En bref, la LSF est très riche et permet une communication très complète,<br> qui viens en quelque sorte combler par le visuel le manque à gagner auditif.
+            </p>
        </div>
 
-   <script src="main.js"></script>
+            <div class="test">
+                <h2>incrustation vidéo</h2>
+                <p class="test_para">
+                Même si on est très loin d'une pratique systématique, on a tous déjà vu des vidéos,<br> notamment de discours officiels,
+                avec incrustation dans un coin de l'écran d'une<br> personne traduisant les dialogues en temps réel. 
+                <br><br>Comme vu précédemment, cela permet a priori de retranscrire plus fidèlement le <br>discours que par les sous-titres.
+                En effet, et notamment dans le cadre d'une<br> traduction instantanée, dure d'imaginer retranscrire par des  sous-titres,<br>
+                les subtilités de la langue parlée.
+                Tout simplement car l'écrit n'est pas fait<br> pour cela : le descriptif complet des subtilités du language oral, incluant<br> les intonations par exemple, serait trop long pour du temps réel.
+                </p>
+          </div>
+
+                <div class="test">
+                <h2>applications utilisant la langue des signes</h2>
+                <p class="test_para">
+                Pour l'instant, il n'existe pas ou peut d'application pour sourds utilisant la langue<br> des signes.
+                Il existe en revanche des  applications de sous-titrage (comme<br> rogervoice ou ava)
+                ou des applications de traîtement de son (a destination <br>des mal-entendants).
+                <br><br>Cela est sans doute dû au fait que retranscrire une conversationen LSF via un  <br>algorithme serait
+                techniquement plus compliquer que  généré des sous-titres :<br>  en effet, il faudrait pour cela analyser
+                des information autre que les seuls mots,<br>  et la traduction passerait non-pas par des caractères mais
+                par exemple par une<br> animation complexe, impliquant des mouvement de main mais aussi de tout 
+                <br>le corps, incluant des expressions faciales.
+                </p> 
+             </div> 
+
+                <div class="test">
+                <h2>idées pour le projet << accessibilité numérique >></h2>
+                
+                <p class="test_para"> Une idée serait de créer une interface semblable à celle d'un traducteur
+                comme <br>celui de google, qui traduirait en alphabet dactylologique.
+                <br><br>Celui-ci serait principalement à destination des personnes entendantes ayant <br>
+                besoin d'un coup de pouce pour communiquer avec une personne sourde.
+                <br><br>Un tel traducteur permettrait de constituer un mot bien plus rapidement qu'en 
+                <br>ayant sous les yeux l'habituel fiche alphabétique.
+                </p>
+            </div>
+
+            <p class="test_para">
+                <h3>Source</h3><br>
+            <a href="https://lejournal.cnrs.fr/articles/faciliter-la-traduction-en-langue-des-signes">https://lejournal.cnrs.fr/articles/faciliter-la-traduction-en-langue-des-signes</a><br>
+            <a href="http://lsq.uqam.ca/sites/default/files/Banff2003.pdf">http://lsq.uqam.ca/sites/default/files/Banff2003.pdf</a><br>
+                <a href="https://fr.wikipedia.org/wiki/Langue_des_signes_française">https://fr.wikipedia.org/wiki/Langue_des_signes_française</a><br>
+                <a href="https://webzine.okeenea.com/6-applications-sourds-malentendants-2020/">https://webzine.okeenea.com/6-applications-sourds-malentendants-2020/</a>
+            </p>
+        </div>
+    <script src="main.js"></script>
 </body>
 </html>
