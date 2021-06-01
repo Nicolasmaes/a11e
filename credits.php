4c563e56b99bbba13957aeff1af0f80429696fe9
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-06-01 16:56:11 +0200

    update credits

---
+++ credits.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <div class="margin">
+        <div class="main__credits">
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">
+                    Sources d'inspiration graphique
+                </span>
+                <a href="https://www.elioz.fr/">Elioz</a>
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">
+                    Sources pour les articles
+                </span>
+
+                <a href="https://lejournal.cnrs.fr/articles/faciliter-la-traduction-en-langue-des-signes">Faciliter la traduction en langue des signes</a>
+
+                <a href="http://lsq.uqam.ca/sites/default/files/Banff2003.pdf">Le bilinguisme sourd</a>
+
+                <a href="https://fr.wikipedia.org/wiki/Langue_des_signes_fran%C3%A7aise">Langue des signes française</a>
+
+                <a href="https://webzine.okeenea.com/6-applications-sourds-malentendants-2020/">6 applications indispensables pour les sourds et malentendants</a>
+
+                <a href="http://babylon-design.com/surdite-accessibilite-sourds-sur-internet/">Babylon Design</a>
+
+                <a href="https://www.bloghoptoys.fr/masques-un-picto-pour-les-malentendants">Un masque pour les maltentendants</a>
+
+                <a href="https://www.bloghoptoys.fr/masques-un-picto-pour-les-malentendants">Un masque pour les maltentendants</a>
+
+                <a href="https://www.amberscript.com/fr/accessibilite-numerique/">Amber Script</a>
+
+                <a href="https://www.francetvinfo.fr/internet/telephonie/avec-l-application-rogervoice-les-personnes-sourdes-et-malentendantes-peuvent-appeler-avec-leur-telephone_2975031.html">RogerVoice</a>
+
+                <a href="https://en.wikipedia.org/wiki/Closed_captioning">Closed captioning</a>
+
+                <a href="https://www.youtube.com/watch?v=DJcTeHsZ4m4">Buster Keaton</a>
+
+                <a href="https://support.google.com/youtube/answer/6054623?hl=fr">(Contribution de la communauté)</a>
+
+                <a href="https://www.freepik.com/photos/hand">(ilustration principale)</a>
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">
+                    Créatrice de la police d'écriture du logo 'Indie Flower'
+                </span>
+
+                <a href="http://www.kimberlygeswein.com/">KImberley Geswein</a>
+
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">
+                    Remerciements
+                </span>
+
+                <a href="https://www.onlineformapro.com/">Onlineformapro</a>
+
+                <a href="https://www.accesscodeschool.fr/">Access Code School</a>
+
+                <a href="http://www.rssb.fr/">Réseau Sourds Santé Bourgogne</a>
+
+                <a href="http://www.visuel-lsf.org/">visuel-lsf</a>
+
+                <a href="https://www.addic7ed.com/">addic7ed</a>
+
+            </p>
+
+        </div>
+    </div>
+    
+</body>
+</html>
