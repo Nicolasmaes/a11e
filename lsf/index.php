dbabc0279a88f1f3fd3ae13524ccf0ad203e9479
sonia <s.rolland@codeur.online>
2021-06-01 14:34:09 +0200

    changement

--- lsf/index.php
+++ lsf/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Language des signes</title>
-    <link rel="stylesheet" media="screen" href="style.css">
+    <link rel="stylesheet"  href="style.css">
 </head>
 <body>
         <div class="main_lsf">
@@ -34,7 +34,7 @@
             <div class="test">
             <h2>spécificités de la LSF</h2>
         
-            <h4>Alphabet dactylologique</h4>
+            <h3>Alphabet dactylologique</h3>
             <p class="test_para">
                 L’alphabet dactylologique est la façon de signer l’alphabet latin. Il est utilisé pour épeler<br> les noms propres ou les mots n’existant pas encore en LSF.La dactylologie de la<br> LSF se fait d’une seule main.
             </p>
@@ -46,34 +46,33 @@
             <h2>grammaire de la LSF</h2>
             <p class="test_para">
                 La langue des signes française permet d'exprimer plusieurs idées simultanément,ce qui<br> la différencie de la grammaire française classique.
-            <li>-L'expressions du visage peut indiquer le sens de la phrase (par exemple pour <br>dire qu'il s'agit d'une question).</li>
-            <li>-Il n’existe pas de conjugaison en LSF, il suffit au signeur de situer l’action sur<br> la ligne du temps 
-            (perpendiculaire à lui:  derrière son épaule le passé,<br> au niveau de son corps le présent et devant lui le futur).</li>
-            <li>-Des gestes montres le rôle ou la forme du référent: Pour faire  référence à un <br>homme, le locuteur utilisera son index vers le  haut,mais pour parler d’une <br>voiture, il utilisera la main à plat. Les verbes de mouvement peuvent <br>également être  signés pour indiquer  la direction :
+            <br>-L'expressions du visage peut indiquer le sens de la phrase (par exemple pour <br>dire qu'il s'agit d'une question).<br>
+            <br>-Il n’existe pas de conjugaison en LSF, il suffit au signeur de situer l’action sur<br> la ligne du temps 
+            (perpendiculaire à lui:  derrière son épaule le passé,<br> au niveau de son corps le présent et devant lui le futur).<br>
+            <br>-Des gestes montres le rôle ou la forme du référent: Pour faire  référence à un <br>homme, le locuteur utilisera son index vers le  haut,mais pour parler d’une <br>voiture, il utilisera la main à plat. Les verbes de mouvement peuvent <br>également être  signés pour indiquer  la direction :
                 « La voiture tourne à <br>gauche »  sera le classificateur de voiture tournant à gauche.<br> -Autre exemple,« Un homme monte l’escalier » se fera par deux doigts
-                imitant<br> les jambes montant un escalier, dans un mouvement pouvant être différent<br> selon qu’il s’agit d’un escalier classique ou en colimaçon.</li>
-            <li>-Pour parler de personnes absentes le signeur définit sa place et celle des autres <br>dans l’espace, à la  manière d’une mise en scène théâtrale, il est ainsi plus facile<br> de  comprendre et de suivre visuellement de qui il s’agit et quels sont les <br>rapports entre les personnages.</li><br>
+                imitant<br> les jambes montant un escalier, dans un mouvement pouvant être différent<br> selon qu’il s’agit d’un escalier classique ou en colimaçon.<br>
+            <br>-Pour parler de personnes absentes le signeur définit sa place et celle des autres <br>dans l’espace, à la  manière d’une mise en scène théâtrale, il est ainsi plus facile<br> de  comprendre et de suivre visuellement de qui il s’agit et quels sont les <br>rapports entre les personnages.br><br>
            En bref, la LSF est très riche et permet une communication très complète,<br> qui viens en quelque sorte combler par le visuel le manque à gagner auditif.
             </p>
        </div>
 
             <div class="test">
                 <h2>incrustation vidéo</h2>
-                <p class="test_para">
-                Même si on est très loin d'une pratique systématique, on a tous déjà vu des vidéos,<br> notamment de discours officiels,
-                avec incrustation dans un coin de l'écran d'une<br> personne traduisant les dialogues en temps réel. 
-                <br><br>Comme vu précédemment, cela permet a priori de retranscrire plus fidèlement le <br>discours que par les sous-titres.
-                En effet, et notamment dans le cadre d'une<br> traduction instantanée, dure d'imaginer retranscrire par des  sous-titres,<br>
-                les subtilités de la langue parlée.
+             <p class="test_para">
+                Même si on est très loin d'une pratique systématique, on a tous déjà vu des vidéos, notamment<br> de discours officiels,avec incrustation dans un coin de l'écran <br>d'une personne traduisant les dialogues en temps réel. 
+             <br><br>Comme vu précédemment, cela permet a priori de retranscrire plus fidèlement le 
+                <br>discours que  par les sous-titres.
+                En effet, et notamment dans le cadre d'une traduction <br>instantanée, dure d'imaginer retranscrire par des  sous-titres,<br>les subtilités de la langue parlée.
                 Tout simplement car l'écrit n'est pas fait<br> pour cela : le descriptif complet des subtilités du language oral, incluant<br> les intonations par exemple, serait trop long pour du temps réel.
                 </p>
           </div>
 
                 <div class="test">
                 <h2>applications utilisant la langue des signes</h2>
-                <p class="test_para">
-                Pour l'instant, il n'existe pas ou peut d'application pour sourds utilisant la langue<br> des signes.
-                Il existe en revanche des  applications de sous-titrage (comme<br> rogervoice ou ava)
+            <p class="test_para">
+                Pour l'instant, il n'existe pas ou peut d'application pour sourds utilisant la langue des<br> signes.
+                Il existe en revanche des  applications de sous-titrage (comme rogervoice <br>ou ava)
                 ou des applications de traîtement de son (a destination <br>des mal-entendants).
                 <br><br>Cela est sans doute dû au fait que retranscrire une conversationen LSF via un  <br>algorithme serait
                 techniquement plus compliquer que  généré des sous-titres :<br>  en effet, il faudrait pour cela analyser
@@ -86,12 +85,12 @@
                 <div class="test">
                 <h2>idées pour le projet << accessibilité numérique >></h2>
                 
-                <p class="test_para"> Une idée serait de créer une interface semblable à celle d'un traducteur
-                comme <br>celui de google, qui traduirait en alphabet dactylologique.
-                <br><br>Celui-ci serait principalement à destination des personnes entendantes ayant <br>
-                besoin d'un coup de pouce pour communiquer avec une personne sourde.
-                <br><br>Un tel traducteur permettrait de constituer un mot bien plus rapidement qu'en 
-                <br>ayant sous les yeux l'habituel fiche alphabétique.
+             <p class="test_para"> Une idée serait de créer une interface semblable à celle d'un traducteur
+                comme celui<br> de google, qui traduirait en alphabet dactylologique.
+             <br><br>Celui-ci serait principalement à destination des personnes entendantes ayant 
+                besoin<br> d'un coup de pouce pour communiquer avec une personne sourde.
+             <br><br>Un tel traducteur permettrait de constituer un mot bien plus rapidement qu'en 
+               ayant <br> sous les yeux l'habituel fiche alphabétique.
                 </p>
             </div>
 
