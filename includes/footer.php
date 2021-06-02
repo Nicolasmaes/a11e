ac418149f1d6c8733a10bb593a1898d8d8e89d0b
luc <l.leneuf@codeur.online>
2021-06-02 16:15:04 +0200

    style+footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,17 @@
+<footer>
+        <img src="../assets/images/pictures/Sans-titre-2.png" alt="arrière plan bleu">
+        <div class="footer_menu">
+            <span>Crédits</span>
+            <span>Mentions légales</span>
+            <span>A propos</span>
+        </div>
+    </footer>
+
+    <script src="../scripts/nav_mobile.js"></script>
+
+</body>
+</html>
+
 <script src="../assets/scripts/main.js"></script>
 </body>
 </html>
