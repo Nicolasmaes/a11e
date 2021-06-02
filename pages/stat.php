b296fd412e1eb67bc17056cca3912d34fe5ab767
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-06-02 16:58:05 +0200

    update statistique

--- pages/stat.php
+++ pages/stat.php
@@ -1,25 +1,47 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="statistique obtenu ">
-        <title>statistique</title>
-        <link rel="stylesheet" href="styles/stat.css">
-    </head>
-    <body>
+<?php include "../includes/header.php" ?>
+
+    <div class="test">
+        <img src="../assets/images/pictures/deafgroup.png" alt="" >
+        <p class="text_interview">Statistiques</p>
+
+        <header>
+            <img src="../assets/images/icons/logo-a11e-white.svg" alt="">
+            <div class="menu_button">
+                <div class="cross">x</div>
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div class="menu_mobile">
+                <a href="interview.html">Interview</a>
+                <span>Statistiques</span>
+                <span>LSF</span>
+                <a href="sous_titres.html">Sous-titres</a>
+            </div>
+            <div class="menu_pc">
+                <span>Interview</span>
+                <span>Statistiques</span>
+                <span>LSF</span>
+                <span>Sous-titre</span>
+            </div>
+        </header>
+    </div>
+
+        <div class="margin">
+            
         <div class="main__statistique">
-            <span class="main__statistique__title">Une France plus sourde qu’on ne le croit !</span>
+
+            <p class="main__statistique__title_mobile">Statistiques</p>
+
+            <h1>Une France plus sourde qu’on ne le croit !</h1>
             
             <p class="main__statistique__paragraphe">
-                <span class="main__statistique__paragraphe__subtitle">
-                    Des chiffres globaux
-                </span><br> <br>
+                <span style="font-weight: bold;">Des chiffres globaux</span><br><br>
 
                 Si 182 000 personnes se définissent comme ayant une surdité complète (0.3 % de la population totale), plus de 7 millions de personnes reconnaissent avoir au moins une déficience auditive soit 11.2 % des français.<br>  
-
-                Autour de 11.5 % des personnes reconnaissent avoir des difficultés à entendre une conversation avec plusieurs personnes.<br>
+                Autour de 11.5 % des personnes reconnaissent avoir des difficultés à entendre une conversation avec plusieurs personnes.<br><br>
 
                 En prenant l’angle des limitations fonctionnelles auditives, l’enquête permet d’établir une estimation de 10 millions de personnes concernées. <br><br> 
                     
@@ -42,57 +64,43 @@
             </p>
             
             <p class="main__statistique__paragraphe">
-                <span class="main__statistique__paragraphe__subtitle">
-                    L’appareillage
-                </span><br> <br>
+                <span style="font-weight: bold;">L’appareillage</span><br><br>
 
-                20 % des personnes ayant des limitations fonctionnelles auditives moyennes à totales sont équipées d’un appareil auditif.<br><br>
-
-                Plus de 2 millions de personnes ne portent pas d’appareils mais en auraient besoin.<br><br>
-
+                20 % des personnes ayant des limitations fonctionnelles auditives moyennes à totales sont équipées d’un appareil auditif. Plus de 2 millions de personnes ne portent pas d’appareils mais en auraient besoin.<br><br>
                 L’enquête note une forte augmentation du taux d’équipement entre 1998 et 2008. Les personnes de moins de 20 ans sont plus souvent équipées, à limitation fonctionnelle égale.
             </p>
             
             <p class="main__statistique__paragraphe">
-                <span class="main__statistique__paragraphe__subtitle">
-                    Vie sociale
-                </span><br> <br>
+                <span style="font-weight: bold;">Vie sociale</span><br><br>
 
                 L’enquête ne note pas de différences significatives sur la vie affective en cas de troubles auditifs, ni de fortes entraves sur les relations familiales ou amicales.<br><br> 
-
                 Les pratiques de loisirs sont semblables entre personnes ayant des déficiences auditives et personnes n’ayant aucune difficulté. Seules les personnes ayant des limitations fonctionnelles très graves à totales semblent avoir une activité de loisirs plus réduite. Cette donnée est à corréler avec l’âge et le niveau de diplômes des personnes interrogées.<br><br>
-
                 L’enquête relève toutefois une forte proportion de jeunes sourds de naissance ne pratiquant pas de sport, ce qui pourrait indiquer des difficultés d’insertion sociale.
             </p>
             
             <p class="main__statistique__paragraphe">
-                <span class="main__statistique__paragraphe__subtitle">
-                    Le cinéma
-                </span><br> <br>
+                <span style="font-weight: bold;">Le cinéma</span><br><br>
 
-                Vive le cinéma étranger ! Cela peut paraître un paradoxe, mais le cinéma français est quasiment inaccessible aux sourds.<br><br>
-
-                Les seuls films disponibles avec des sous-titrages sont les films en langues étrangères diffusés en version originale. Par définition, en France on ne sous-titre pas les films en langue française au dépends des sourds. <br><br>
-
+                Vive le cinéma étranger ! Cela peut paraître un paradoxe, mais le cinéma français est quasiment inaccessible aux sourds. Les seuls films disponibles avec des sous-titrages sont les films en langues étrangères diffusés en version originale. Par définition, en France on ne sous-titre pas les films en langue française au dépends des sourds. <br><br>
                 En outre, à ma connaissance, mis à part à Paris, il existe très peu de salles diffusant des films en version originale. Et par conséquent, ce n’est pas un hasard que les sourds désertent les salles obscures en France.  
             </p>
             
             <p class="main__statistique__paragraphe">
-                <span class="main__statistique__paragraphe__subtitle">
-                    Situation administrative
-                </span><br> <br>
+                <span style="font-weight: bold;">Situation administrative</span><br><br>
 
                 Parmi les personnes ayant des limitations auditives moyennes à totales, 16 % déclarent avoir une reconnaissance administrative de leur handicap (taux d’incapacité reconnu par la MDPH, ou taux d’invalidité octroyé par la Sécurité Sociale par exemple).<br><br>  
-
                 Les personnes de moins de 60 ans et ayant une limitation fonctionnelle auditive ont plus souvent une reconnaissance administrative du handicap. A difficultés égales, les femmes ont une plus faible reconnaissance de leur handicap.<br><br>
+                4 % des personnes ayant des limitations fonctionnelles moyennes à totales touchent une pension d’invalidité. 5 % des personnes entre 20 et 59 ans et ayant des limitations fonctionnelles moyennes à totales déclarent recevoir l’Allocation Adulte Handicapé (AAH).
+            </p>
 
-                4 % des personnes ayant des limitations fonctionnelles moyennes à totales touchent une pension d’invalidité. 5 % des personnes entre 20 et 59 ans et ayant des limitations fonctionnelles moyennes à totales déclarent recevoir l’Allocation Adulte Handicapé (AAH). <br><br>
+            <p class="main__statistique__paragraphe">
+                <span style="font-weight: bold;">Liens utilisé :</span> <br><br>
 
-                Liens utilisé : <br><br>
-                <a href="https://www.surdi.info/bibliographie/des-chiffres-autour-de-la-surdite/#:~:text=Des%20chiffres%20globaux,auditive%20soit%2011.2%20%25%20des%20fran%C3%A7ais.">Des chiffres autour de la surdité</a>
+                <a style="font-style: italic;" href="https://www.surdi.info/bibliographie/des-chiffres-autour-de-la-surdite/#:~:text=Des%20chiffres%20globaux,auditive%20soit%2011.2%20%25%20des%20fran%C3%A7ais. ">Des chiffres autour de la surdité</a>
             </p>
            
         </div>
+
+    </div>
         
-    </body>
-</html>
+<?php include "../includes/footer.php" ?>
