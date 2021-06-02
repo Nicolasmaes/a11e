11c11f48fc6b313b581176b54e0c041a9dba1627
sonia <s.rolland@codeur.online>
2021-06-02 15:03:27 +0200

    restructuration

--- pages/lsf.php
+++ pages/lsf.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Language des signes</title>
-    <link rel="stylesheet"  href="style.css">
-</head>
-<body>
+<?php include '../includes/header.php' ?>
 <div class="margin">
         <div class="main__lsf">
             <div class="main__lsf__paragraphe">
@@ -105,6 +96,4 @@
             <p>
         </div>
         </div>
-    <script src="main.js"></script>
-</body>
-</html>
+<?php  include '../includes/footer.php' ?>
