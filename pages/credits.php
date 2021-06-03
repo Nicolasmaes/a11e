1a8887d216d99cebd82b351cfc7abf5ae0bc211e
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-06-03 14:54:10 +0200

    final version

---
+++ pages/credits.php
@@ -0,0 +1,79 @@
+<?php include "../includes/header.php"; ?>
+
+<div class="test">
+        <img src="../assets/images/pictures/deafgroup.png" alt="" >
+        <p class="text_sources">Sources</p>
+
+        <header>
+            <img src="../assets/images/icons/logo-a11e-white.svg" alt="">
+            <div class="menu_button">
+                <div class="cross">x</div>
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div class="menu_mobile">
+                <span>Interview</span>
+                <span>Statistiques</span>
+                <span>LSF</span>
+                <span>Sous-titre</span>
+            </div>
+            <div class="menu_pc">
+                <span>Interview</span>
+                <span>Statistiques</span>
+                <span>LSF</span>
+                <span>Sous-titre</span>
+            </div>
+        </header>
+    </div>
+
+    <div class="margin">
+        <div class="main__credits">
+
+            <p class="main__credits__title_mobile">Sources</p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">Sources d'inspiration graphique</span> <br><br>
+
+                <a href="https://www.elioz.fr/">Elioz</a>
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">Sources pour les articles</span> <br><br>
+
+                <a href="https://lejournal.cnrs.fr/articles/faciliter-la-traduction-en-langue-des-signes">Faciliter la traduction en langue des signes</a><br>
+                <a href="http://lsq.uqam.ca/sites/default/files/Banff2003.pdf">Le bilinguisme sourd</a><br>
+                <a href="https://fr.wikipedia.org/wiki/Langue_des_signes_fran%C3%A7aise">Langue des signes française</a> <br>
+                <a href="https://webzine.okeenea.com/6-applications-sourds-malentendants-2020/">6 applications indispensables pour les sourds et malentendants</a><br>
+                <a href="http://babylon-design.com/surdite-accessibilite-sourds-sur-internet/">Babylon Design</a><br>
+                <a href="https://www.bloghoptoys.fr/masques-un-picto-pour-les-malentendants">Un masque pour les maltentendants</a><br>
+                <a href="https://www.bloghoptoys.fr/masques-un-picto-pour-les-malentendants">Un masque pour les maltentendants</a><br>
+                <a href="https://www.amberscript.com/fr/accessibilite-numerique/">Amber Script</a><br>
+                <a href="https://www.francetvinfo.fr/internet/telephonie/avec-l-application-rogervoice-les-personnes-sourdes-et-malentendantes-peuvent-appeler-avec-leur-telephone_2975031.html">RogerVoice</a><br>
+                <a href="https://en.wikipedia.org/wiki/Closed_captioning">Closed captioning</a><br>
+                <a href="https://www.youtube.com/watch?v=DJcTeHsZ4m4">Buster Keaton</a><br>
+                <a href="https://support.google.com/youtube/answer/6054623?hl=fr">(Contribution de la communauté)</a><br>
+                <a href="https://www.freepik.com/photos/hand">(ilustration principale)</a>
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">Créatrice de la police d'écriture du logo 'Indie Flower'</span><br><br>
+
+                <a href="http://www.kimberlygeswein.com/">KImberley Geswein</a>
+            </p>
+
+            <p class="main__credits__paragraphe">
+                <span style="font-weight: bold;">Remerciements</span><br><br>
+
+                <a href="https://www.onlineformapro.com/">Onlineformapro</a>
+                <a href="https://www.accesscodeschool.fr/">Access Code School</a>
+                <a href="http://www.rssb.fr/">Réseau Sourds Santé Bourgogne</a>
+                <a href="http://www.visuel-lsf.org/">visuel-lsf</a>
+                <a href="https://www.addic7ed.com/">addic7ed</a>
+            </p>
+        </div>
+    </div>
+
+<?php include "../includes/footer.php"; ?>
