fe756611d658adcdd622f688d29da0bf6d72ef99
luc <l.leneuf@codeur.online>
2021-06-03 14:25:08 +0200

    up

--- pages/landing_page.php
+++ pages/landing_page.php
@@ -1,45 +1,59 @@
 <?php include "../includes/header.php";?>
  
 
-  
-    <div class="test">
+<div class="test">
         <img src="../assets/images/pictures/deafgroup.png" alt="" >
         <p class="text_landingpage">
             Découvrons ensemble une communauté qui n'est pas assez entendue
         </p>
+
         <header>
-            <img src="../assets/images/icons/logo-a11e-white.svg" alt="logo">
-            <div class="menu">
+            <img src="../assets/images/icons/logo-a11e-white.svg" alt="">
+
+            <div class="menu_button">
+                <div class="cross">x</div>
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div class="menu_mobile">
                 <span>Interview</span>
                 <span>Statistiques</span>
                 <span>LSF</span>
                 <span>Sous-titre</span>
             </div>
 
-            <div class="menu2">
+            <div class="menu_pc">
                 <span>Interview</span>
                 <span>Statistiques</span>
                 <span>LSF</span>
                 <span>Sous-titre</span>
             </div>
-            
         </header>
     </div>
 
     <div class="margin">
 
-        <main>
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Sf7mLlImkYI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <main class="main__landing_page">
 
-            <div class="miniature">
-                    <div class="miniature__group">
-                        <p class="miniature__group__text">
-                            <span class="miniature__group__text__titre">Interview</span> <br>
-                            <span class="miniature__group__text__description">A l'écoute des sourds</span>
+            <p class="main__landing_page__title_mobile">
+                Découvrons ensemble une communauté qui n'est pas assez entendue
+            </p>
+  
+            <iframe src="https://www.youtube.com/embed/Sf7mLlImkYI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <div class="main__landing_page__miniature">
+                    <div class="main__landing_page__miniature__group">
+                        <p class="main__landing_page__miniature__group__text">
+                            <span class="main__landing_page__miniature__group__text__titre">Interview</span> <br>
+                            <span class="main__landing_page__miniature__group__text__description">A l'écoute des sourds</span>
                         </p>
 
                         <a href="#">
-                            <div class="miniature__group__button"></div>
+                            <div class="main__landing_page__miniature__group__button">Parcourir</div>
                         </a>
                     </div>
 
@@ -47,15 +61,15 @@
             </div>
 
 
-            <div class="miniature">
-                    <div class="miniature__group">
-                        <p class="miniature__group__text">
-                            <span class="miniature__group__text__titre">Statistiques</span> <br>
+            <div class="main__landing_page__miniature">
+                    <div class="main__landing_page__miniature__group">
+                        <p class="main__landing_page__miniature__group__text">
+                            <span class="main__landing_page__miniature__group__text__titre">Statistiques</span> <br>
                             <span>Et si on parlait chiffres</span>
                         </p>
 
                         <a href="#">
-                            <div class="miniature__group__button"></div>
+                            <div class="main__landing_page__miniature__group__button">Parcourir</div>
                         </a>
                     </div>
 
@@ -63,15 +77,15 @@
             </div>
 
 
-            <div class="miniature">
-                    <div class="miniature__group">
-                        <p class="miniature__group__text">
-                            <span class="miniature__group__text__titre">Sous-titres</span> <br>
+            <div class="main__landing_page__miniature">
+                    <div class="main__landing_page__miniature__group">
+                        <p class="main__landing_page__miniature__group__text">
+                            <span class="main__landing_page__miniature__group__text__titre">Sous-titres</span> <br>
                             <span>Les malentendants en profitent-ils vraiment ?</span>
                         </p>
 
                         <a href="#">
-                            <div class="miniature__group__button"></div>
+                            <div class="main__landing_page__miniature__group__button">Parcourir</div>
                         </a>
                     </div>
 
@@ -79,24 +93,25 @@
             </div>
 
 
-            <div class="miniature">
-                    <div class="miniature__group">
-                        <p class="miniature__group__text">
-                            <span class="miniature__group__text__titre">Langue des signes</span> <br>
+            <div class="main__landing_page__miniature">
+                    <div class="main__landing_page__miniature__group">
+                        <p class="main__landing_page__miniature__group__text">
+                            <span class="main__landing_page__miniature__group__text__titre">Langue des signes</span> <br>
                             <span>Apprenez à nous comprendre</span>
                         </p>
 
                         <a href="#">
-                            <div class="miniature__group__button"></div>
+                            <div class="main__landing_page__miniature__group__button">Parcourir</div>
                         </a>
                     </div>
 
                     <img src="../dossier-conception/futures-assets/logo-landing-page-hand.png" alt="illustration d'une main">
             </div>
+
         </main>
 
     </div>
 
-    <img src="../assets/images/pictures/Sans-titre-2.png" alt="" class="foot">
+    
     
 <?php include "../includes/footer.php";?>
