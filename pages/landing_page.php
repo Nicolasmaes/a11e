3121cb0968e903cc994e4ae161fc083f42dec8c8
luc <l.leneuf@codeur.online>
2021-06-03 13:55:34 +0200

    final version

--- pages/landing_page.php
+++ pages/landing_page.php
@@ -1,14 +1,4 @@
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
-    <link rel="stylesheet" href="../styles/styles.css">
-</head>
-
-<body>
-
+<?php include "../includes/header.php";?>
  
 
   
@@ -109,5 +99,4 @@
 
     <img src="../assets/images/pictures/Sans-titre-2.png" alt="" class="foot">
     
-</body>
-</html>
+<?php include "../includes/footer.php";?>
