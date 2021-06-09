200ca9b13afaa51712603450de74e312f9fecf3c
luc <l.leneuf@codeur.online>
2021-06-09 14:50:46 +0200

    ajout de métadescription

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site web concernant l'accessibilité digital aux sourds et malentendants">
     <title>a11é</title>
     <link rel="stylesheet"  href="../assets/styles/style.css">
 </head>
